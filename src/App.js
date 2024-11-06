851a6527bf67bc9087a7a65cdce2e2806dcdf303
abdulhaihabibi2024 <abdulhaihabibi60@gmail.com>
2024-11-06 10:02:29 +0430

    add table in user table

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ import Authors from './pages/Authors';
 import Categories from './pages/Categories';
 import Users from './pages/Users';
 import Reservations from './pages/Reservations';
+import UserTable from './components/UserTable';
 
+
+const users = [
+  { id: 1, name: "Abdul Hai Habibi", email: "abdulhaihabibi60@gmail.com", phone: "+93794167045" },
+  { id: 2, name: "Hasibullah Alkozay", email: "hasibullahulfat14@gmail.com", phone: "+93780153714" },
+  { id: 2, name: "Abdul Khalid", email: "khalidqafori@gmail.com", phone: "+93748276213" },
+  { id: 2, name: "Rafiullah Qaderi", email: "rafiullahqaderi81@gmail.com", phone: "+93774824265" },
+  { id: 2, name: "Hamidullah Noorzai", email: "n_hamidullah@hotmail.com", phone: "+93772020759" },
+  { id: 2, name: "Mobariz Katawazi", email: "mobariz.Katawazi@gmail.com", phone: "+93798501094" },
+];
 
 const routes=[
   { path:'/',label:"Home"},
@@ -27,7 +37,7 @@ function App() {
             <Route path="/books" element={<Books /> }  />
             <Route path="/authors" element={<Authors />} />
             <Route path="/categories" element={<Categories />}  />
-            <Route path="/users" element={<Users />}  />
+            <Route path="/users" element={<><Users /><UserTable  users={users}/></>}  />
             <Route path="/reservations" element={<Reservations />}  />
           </Routes>
           
---
+++ src/components/UserTable.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+
+const UserTable = ({ users }) => {
+
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Phone</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((user, index) => (
+            <TableRow key={index}>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.phone}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default UserTable;
