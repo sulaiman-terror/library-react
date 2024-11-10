8fdb990a7f1620562fb967e0145671fc49553db6
sulaiman-terror <sulaiman.rf3@gmail.com>
2024-11-10 09:21:57 +0430

    Sulaiman Team tasks updated and pushed

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-export default function User() {
-  return (
-    <div>
-      
-    </div>
-  )
+function Users() {
+const users = [
+{ id: 1, name: 'User A' },
+{ id: 2, name: 'User B' },
+];
+
+return (
+<div>
+  <h2>Users</h2>
+    <ul>
+      {users.map(user => (
+      <li key={user.id}>{user.name}</li>
+      ))}
+  </ul>
+</div>
+);
 }
+
+export default Users;
